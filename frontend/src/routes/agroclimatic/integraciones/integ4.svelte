ed67d0ccd28275778ba6fcc9d3faa9d706c45482
Carlos López <carlopcar4@alumn.us.es>
2023-05-16 19:16:13 +0200

    nose

--- frontend/src/routes/agroclimatic/integraciones/integ4.svelte
+++ frontend/src/routes/agroclimatic/integraciones/integ4.svelte
@@ -1,10 +1,3 @@
-<!-- <svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-</svelte:head> -->
-
 <script>
     // @ts-nocheck
     import { onMount } from "svelte";
