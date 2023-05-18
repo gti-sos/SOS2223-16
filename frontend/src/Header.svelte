ba81d829610f1c7acd52cbe3d9550d2a469999da
Carlos López <carlopcar4@alumn.us.es>
2023-05-18 16:57:12 +0200

    integrations2

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -3,7 +3,7 @@
     <a href="/agroclimatic">agroclimatización</a><br>
     <a href="/professionalorganisations">Estadisticas Colegios Profesionales</a><br>
     <a href="/professionalorganisations/graph">Gráficas Colegios Profesionales</a><br>
-    <a href="/professionalorganisations/integrations">Integraciones y usos Carolina</a><br>
+    <a href="/integrations">Integraciones y usos Carolina</a><br>
     <a href="/civilwarandalusian">civil war andalusian phoses</a><br>
     <a href="/integrations">Gráficas e integraciones</a>
 </nav>
