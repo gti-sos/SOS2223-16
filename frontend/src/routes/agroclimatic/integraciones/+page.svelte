dff34aacb47401dc2c403ea6a8c8180e3dc960be
Carlos López <carlopcar4@alumn.us.es>
2023-05-19 17:26:54 +0200

    terminado proxy 2

--- frontend/src/routes/agroclimatic/integraciones/+page.svelte
+++ frontend/src/routes/agroclimatic/integraciones/+page.svelte
@@ -1,12 +1,12 @@
 <script>
     import bill from "./integ1_Chart.svelte";
-    // import intt from "./integ2_HighChart.svelte";
-    // import int2 from "./integ3_HighChart2.svelte";
-    // import int3 from "./integ4.svelte";
+    import intt from "./integ2_HighChart.svelte";
+    import int2 from "./integ3_HighChart2.svelte";
+    import int3 from "./integ4.svelte";
 </script>
 
 <svelte:component this={bill} />
-<!-- <svelte:component this={intt} />
+<svelte:component this={intt} />
 <svelte:component this={int2} />
-<svelte:component this={int3} /> -->
+<svelte:component this={int3} />
 
