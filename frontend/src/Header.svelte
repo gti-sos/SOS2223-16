290bf426d3e2ddd5280159122ab0ddd1ede3b9bf
Carolina Bernal <carolinabm2000@gmail.com>
2023-05-20 22:54:04 +0200

    Grafica conjunta

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -1,7 +1,7 @@
 <nav>
     <a href="/">Inicio</a><br>
-    <a href="/agroclimatic">agroclimatización</a><br>
+    <a href="/agroclimatic">Agroclimatización</a><br>
     <a href="/professionalorganisations">Estadisticas Colegios Profesionales</a><br>
-    <a href="/civilwarandalusian">civil war andalusian phoses</a><br>
+    <a href="/civilwarandalusian">Civil war andalusian phoses</a><br>
     <a href="/integrations">Gráficas e integraciones</a><br>
 </nav>
--- frontend/src/routes/analytics/+page.svelte
+++ frontend/src/routes/analytics/+page.svelte
@@ -10,41 +10,46 @@
     // @ts-nocheck
     import { onMount } from "svelte";
     import { dev } from "$app/environment";
-    //carlos
-    let API_agric = "/api/v2/cadiz-agroclimatic-informations-stats";
-    if (dev) API_agric = "http://localhost:8080" + API_agric;
-    //Marcos
-    let API_Civil = "/api/v2/civilwarandalusian-stats";
-    if (dev) API_Civil = "http://localhost:8080" + API_Civil;
 
-    let datosAg = null;
-    let error = null;
+    const average = arr => {
+        if(arr.length === 0) return 0;
+        return arr.reduce( ( p, c ) => p + c, 0 ) / arr.length
+    };
+
     let dataAg = null;
+    let dataCw = null;
+    let dataprofessionalOrg = null;
     let responseAg = null;
     let resultAg = "";
     let resultStatusAg = "";
-
     let resultcw = "";
     let resultStatuscw = "";
-    let datoscw = null;
+    let resultprofessionalOrg = "";
+    let resultStatusprofessionalOrg = "";
+    let years = new Set();
 
     onMount(async () => {
-        await getDatos();
+        
+        await getProfessionalOrg();   
+        await getAgro();
         await getPhoses();
+        await loadChart();
+
     });
 
     //Carlos
-    async function getDatos(){
-        resultStatusAg = resultAg = "";
+    async function getAgro(){
+        let API_agric = "/api/v2/cadiz-agroclimatic-informations-stats";
+        if (dev) API_agric = "http://localhost:8080" + API_agric;
         const res = await fetch(API_agric, {
             method: "GET",
         });
         try{
-            const dataAg = await res.json();
-            resultAg = JSON.stringify(dataAg, null, 2);
-            datosAg = dataAg;
-            //-----
-            loadChart();
+            dataAg= await res.json();
+            dataAg.forEach((a) => {
+                years.add(a.date);
+            });
+
         } catch (error){
             console.log(`Error parsing result: ${error}`);
         }
@@ -55,55 +60,103 @@
     //Marcos
     async function getPhoses(){
         resultStatuscw = resultcw = "";
-        const res1 = await fetch(API_Civil, {
+        let API_civil = "/api/v2/civilwarandalusian-stats";
+        if (dev) API_civil = "http://localhost:8080" + API_civil;
+        const res = await fetch(API_civil, {
             method: "GET",
         });
         try{
-            const datacw = await res1.json();
-            resultAg = JSON.stringify(datacw, null, 2);
-            datoscw = datacw;
-            //-----
-            loadChart();
+            dataCw= await res.json();
+            dataCw.forEach((a) => {
+                years.add(a.dates_act);
+            });
+
         } catch (error){
             console.log(`Error parsing result: ${error}`);
         }
-        const status1 = await res1.status;
-        resultStatuscw = status1;
+        const status = await res.status;
+        resultStatusAg = status;
+    }
+
+    //Carolina
+    async function getProfessionalOrg(){
+        let API_professionalOrg = "/api/v2/professionalorganisations-stats";
+        if (dev) API_professionalOrg = "http://localhost:8080" + API_professionalOrg;
+        const res = await fetch(API_professionalOrg, {
+            method: "GET",
+        });
+        try{
+            dataprofessionalOrg= await res.json();
+            dataprofessionalOrg.forEach((a) => {
+                years.add(a.date);
+            });
+
+        } catch (error){
+            console.log(`Error parsing result: ${error}`);
+        }
+        const status = await res.status;
+        resultStatusprofessionalOrg = status;
     }
     
     
     async function loadChart() {
+        let tempMediaSeries=[];
+        let tempMaxSeries=[];
+        let tempMinSeries=[];
+        let victimDocSeries=[];
+        let victimSeries=[];
+        let victimIdSeries=[];
+        let ProfessionalOrgIdSeries=[];
+        let ProfessionalOrgCpSeries=[];
+        let ProfessionalOrgCount=[];
+
+        //set to array
+        years=[...years].sort();
+        years.forEach((year) => {
+            let dataAgYear = dataAg.filter((a) => a.date === year);
+            let dataCWYear = dataCw.filter((a) => a.dates_act === year);
+            let dataCWVictimYear = dataCw.filter((a) => a.dateNumeric === year);
+            let dataProfessionalOrgYear = dataprofessionalOrg.filter((a) => a.date === year);
+
+            //Carlos
+            tempMediaSeries.push(average(dataAgYear.map((a) => a.averagetemp)));
+            tempMaxSeries.push(average(dataAgYear.map((a) => a.maxtemp)));
+            tempMinSeries.push(average(dataAgYear.map((a) => a.mintemp)));
+
+            //Marcos
+            victimDocSeries.push(average(dataCWYear.map((a) => a.victims)));
+            victimSeries.push(average(dataCWVictimYear.map((a) => a.victims)));
+            victimIdSeries.push(average(dataCWYear.map((a) => a.Id)));
+
+            //Carolina
+            ProfessionalOrgIdSeries.push(average(dataProfessionalOrgYear.map((a) => a.registry_number)));
+            ProfessionalOrgCpSeries.push(average(dataProfessionalOrgYear.map((a) => a.postal_code/1000)));
+            ProfessionalOrgCount.push(dataProfessionalOrgYear.length);
+
+        });
+
+
+        console.log(years);
+        console.log(tempMediaSeries);
+        console.log(victimSeries);
+
         Highcharts.chart("container", {
             chart: {
                 type: "column",
             },
             title: {
-                text: "Estadísticas agroclimáticas-Fosas Guerra Civil",
+                text: "Estadísticas conjuntas",
             },
             subtitle: {
                 text: "Gráfica hecha con HighCharts",
             },
             xAxis: {
                 categories: 
-                    datosAg.map((a) => `${a.reg_num}-${a.location}-${a.stations_id}-${a.date}}`) ,
+                    years ,
                     crosshair: true,
             },
             yAxis: {
-                min: 0,
-                title: {
-                    text: "Grados (ºC)",
-                    //text2: "Fosas",
-                },
-            },
-            tooltip: {
-                headerFormat:
-                    '<span style="font-size:10px">{point.key}</span><table>',
-                pointFormat:
-                    ['<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-                    '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>'] ,
-                footerFormat: "</table>",
-                shared: true,
-                useHTML: true,
+                min: 0
             },
             plotOptions: {
                 column: {
@@ -113,29 +166,42 @@
             },
             series: [
                 {
-                    name: "Máx Temperatura",
-                    data: datosAg.map(a => a.maxtemp),
+                    name: "Temperatura máxima media por año",
+                    data: tempMaxSeries,
                 },
                 {
-                    name: "Min Temperatura",
-                    data: datosAg.map(a => a.mintemp),
+                    name: "Temperatura minima media por año",
+                    data: tempMinSeries,
                 },
                 {
-                    name: "Temperatura Media",
-                    data: datosAg.map(a => a.averagetemp),
+                    name: "Temperatura media por año",
+                    data: tempMediaSeries,
                 },
                 {
-                    name: "victimas",
-                    data: datoscw.map(a => a.victims),
+                    name: "Victimas medias por año (fecha del documento)",
+                    data: victimDocSeries,
                 },
                 {
-                    name: "Diferencia acontecimiento-actualizacion",
-                    data: datoscw.map(a => (a.dates_act-a.dateNumeric)),
+                    name: "Victimas medias por año (fecha del hecho)",
+                    data: victimSeries,
                 },
                 {
-                    name: "Identificador",
-                    data: datoscw.map(a => a.Id),
-                },                
+                    name: "Id media de documento de victima por año",
+                    data: victimIdSeries,
+                },
+                {
+                    name: "Id media de colegios profesionales por año",
+                    data: ProfessionalOrgIdSeries,
+                },
+                {
+                    name: "Media de los códigos postales (dos primeros dígitos) de los colegios profesionales por año",
+                    data: ProfessionalOrgCpSeries,
+                },
+                {
+                    name: "Numero de los colegios profesionales por año",
+                    data: ProfessionalOrgCount,
+                }
+                
             ],
         });
     }
@@ -145,10 +211,6 @@
 <main>
     <figure class="highcharts-figure">
         <div id="container"></div>
-        <h1 class="highcharts-description">
-         Estadísticas agroclimáticas
-        </h1>
-        <h5>Temperatura máxima por año</h5>
     </figure>
     <div id="Chart"></div>
     
