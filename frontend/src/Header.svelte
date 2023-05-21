aeb9001c73eb234a673d425558a9b6216222ab10
Carlos López <carlopcar4@alumn.us.es>
2023-05-21 08:54:44 +0200

    final

--- frontend/src/Header.svelte
+++ frontend/src/Header.svelte
@@ -4,4 +4,5 @@
     <a href="/professionalorganisations">Estadisticas Colegios Profesionales</a><br>
     <a href="/civilwarandalusian">Civil war andalusian phoses</a><br>
     <a href="/integrations">Gráficas e integraciones</a><br>
+    <a href="/efforts">esfuerzo</a><br>
 </nav>
