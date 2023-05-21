30e07884e9182283504e18f3e96dd4bb4ae9d662
Carolina Bernal <carolinabm2000@gmail.com>
2023-05-21 12:39:20 +0200

    mejoras en el css

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -2,30 +2,52 @@
     <title>Inicio</title>
 </svelte:head>
 
-<body style="text-align: center">
-<h1>SOS2223-16</h1>
+    <style>
+        body {
+            text-align: center;
+        }
+        h1 {
+            color: blue;
+            font-weight: bolder;
+        }
+        h2 {
+                color: green;
+        }
+        ul {
+            list-style-type: none;
+            padding: 0;
+        }
+        li {
+            margin-bottom: 10px;
+        }
+        p {
+            font-style: italic;
+            margin-top: 5px;
+        }
+    </style>
 
-<h2>Repositorio github:</h2>
-<a href="https://github.com/gti-sos/SOS2223-16">gti-sos/SOS2223-16</a>
-
-<h2>Equipo</h2>
-<ul>
-    <a href="https://github.com/CarolinaBernalMarchena">Carolina Bernal Marchena</a><p>Fuente de datos: professionalorganisations-stats</p>
-    <a href="https://github.com/carlopcar4">Carlos López Cárdenas</a><p>Fuente de datos: cadiz-agroclimatic-informations-stats</p>
-    <a href="https://github.com/marcos1812">Marcos Morales Campos</a><p>Fuente de datos: civilwarandalusian-stats</p>
-</ul>
-
-<h2>APIs</h2>
-<ul><a href="https://sos2223-16.appspot.com/api/v1/professionalorganisations-stats">professionalorganisations-stats</a></ul>
-<ul><a href="https://sos2223-16.appspot.com/api/v1/cadiz-agroclimatic-informations-stats">cadiz-agroclimatic-informations-stats</a></ul>
-<ul><a href="https://sos2223-16.appspot.com/api/v1/civilwarandalusian-stats">civilwarandalusian-stats</a></ul>
-
-<h2>Documentación en Postman</h2>
-<ul>
-    <a href="https://sos2223-16.appspot.com/api/v1/professionalorganisations-stats/docs">https://sos2223-16.appspot.com/api/v1/professionalorganisations-stats/docs</a><p>Desarrollado por Carolina Bernal Marchena</p>
-    <a href="https://sos2223-16.appspot.com/api/v2/professionalorganisations-stats/docs">https://sos2223-16.appspot.com/api/v2/professionalorganisations-stats/docs</a><p>Desarrollado por Carolina Bernal Marchena</p>
-    <a href="https://documenter.getpostman.com/view/26036256/2s93Xwy47C">https://sos2223-16.ew.r.appspot.com/api/v1/cadiz-agroclimatic-informations-stats/docs</a><p>Desarrollado por Carlos López Cárdenas (v1)</p>
-    <a href="https://documenter.getpostman.com/view/26036256/2s93Xwy47A">https://sos2223-16.ew.r.appspot.com/api/v2/cadiz-agroclimatic-informations-stats/docs</a><p>Desarrollado por Carlos López Cárdenas (v2)</p>
-    <a href="https://sos2223-16.ew.r.appspot.com/api/v1/civilwarandalusian-stats/docs">https://sos2223-16.ew.r.appspot.com/api/v1/civilWarAndalusian-stats/docs</a><p>Desarrollado por Marcos Morales Campos</p>
-<ul>
+<body>
+    <h1>SOS2223-16</h1>
+    <h2>Repositorio GitHub:</h2>
+    <a href="https://github.com/gti-sos/SOS2223-16">gti-sos/SOS2223-16</a>
+    <h2>Equipo</h2>
+    <ul>
+        <li><a href="https://github.com/CarolinaBernalMarchena">Carolina Bernal Marchena</a><br><p>Fuente de datos: professionalorganisations-stats</p></li>
+        <li><a href="https://github.com/carlopcar4">Carlos López Cárdenas</a><br><p>Fuente de datos: cadiz-agroclimatic-informations-stats</p></li>
+        <li><a href="https://github.com/marcos1812">Marcos Morales Campos</a><br><p>Fuente de datos: civilwarandalusian-stats</p></li>
+    </ul>
+    <h2>APIs</h2>
+    <ul>
+        <li><a href="https://sos2223-16.appspot.com/api/v1/professionalorganisations-stats">professionalorganisations-stats</a></li>
+        <li><a href="https://sos2223-16.appspot.com/api/v1/cadiz-agroclimatic-informations-stats">cadiz-agroclimatic-informations-stats</a></li>
+        <li><a href="https://sos2223-16.appspot.com/api/v1/civilwarandalusian-stats">civilwarandalusian-stats</a></li>
+    </ul>
+    <h2>Documentación en Postman</h2>
+    <ul>
+        <li><a href="https://sos2223-16.appspot.com/api/v1/professionalorganisations-stats/docs">professionalorganisations-stats (v1)</a><br><p>Desarrollado por Carolina Bernal Marchena</p></li>
+        <li><a href="https://sos2223-16.appspot.com/api/v2/professionalorganisations-stats/docs">professionalorganisations-stats (v2)</a><br><p>Desarrollado por Carolina Bernal Marchena</p></li>
+        <li><a href="https://sos2223-16.appspot.com/api/v1/cadiz-agroclimatic-informations-stats/docs">cadiz-agroclimatic-informations-stats (v1)</a><br><p>Desarrollado por Carlos López Cárdenas</p></li>
+        <li><a href="https://documenter.getpostman.com/view/26036256/2s93Xwy47A">https://sos2223-16.ew.r.appspot.com/api/v2/cadiz-agroclimatic-informations-stats/docs</a><p>Desarrollado por Carlos López Cárdenas (v2)</p></li>
+        <li><a href="https://sos2223-16.ew.r.appspot.com/api/v1/civilwarandalusian-stats/docs">https://sos2223-16.ew.r.appspot.com/api/v1/civilWarAndalusian-stats/docs</a><p>Desarrollado por Marcos Morales Campos</p></li>
+    </ul>
 </body>
--- frontend/src/routes/integrations/+page.svelte
+++ frontend/src/routes/integrations/+page.svelte
@@ -1,3 +1,25 @@
+<style>
+    body {
+        text-align: center;
+    }
+    h1 {
+        color: blue;
+        font-weight: bolder;
+    }
+    h2 {
+        color: green;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    p {
+        font-style: italic;
+        margin-top: 5px;
+    }
+</style>
+
+
 
 <h1>GRÁFICAS E INTEGRACIONES</h1>
 <ul><a href="/analytics">Gráfica conjunta</a></ul>
