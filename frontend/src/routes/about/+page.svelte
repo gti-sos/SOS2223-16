c9f8cb623b15e5e20d43c9200347ba6f977898db
Carlos López <carlopcar4@alumn.us.es>
2023-05-21 12:02:21 +0200

    final

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <nav>
-    Carlos López Cárdenas: <a href="/">Video de agroclimatización</a><br>
+    Carlos López Cárdenas: <a href="https://youtu.be/xf-fBpfgW1o">Video de agroclimatización</a><br>
 </nav>
