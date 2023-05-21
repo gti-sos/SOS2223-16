38802b5de8ca31721f792c25a9b4718a06b22970
Carlos López <carlopcar4@alumn.us.es>
2023-05-21 10:26:35 +0200

    final

--- frontend/src/routes/agroclimatic/integraciones/integ4.svelte
+++ frontend/src/routes/agroclimatic/integraciones/integ4.svelte
@@ -101,6 +101,6 @@ async function loadChart() {
 </script>
 
 <main>
-    <h1>Temperaturas anuales y número de espectadores de anime</h1>
+    <h1>Máximas temperaturas anuales y número de espectadores de anime</h1>
     <div id="chart-container" style="width: 600px; height: 700px;"></div>
 </main>
