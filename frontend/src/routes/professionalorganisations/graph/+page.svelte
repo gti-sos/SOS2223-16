0f5e51f8479ad4703ab5d3aefbfa9e58333eda2d
Carolina Bernal <carolinabm2000@gmail.com>
2023-05-22 00:11:48 +0200

    minor script optimization

--- frontend/src/routes/professionalorganisations/graph/+page.svelte
+++ frontend/src/routes/professionalorganisations/graph/+page.svelte
@@ -1,9 +1,3 @@
-<svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/highcharts-more.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-</svelte:head>
 
 <script>
         // @ts-nocheck
--- frontend/src/routes/professionalorganisations/integrations/+page.svelte
+++ frontend/src/routes/professionalorganisations/integrations/+page.svelte
@@ -1,13 +1,4 @@
-<svelte:head>
-    <script src="https://code.highcharts.com/highcharts.js"></script>
-    <script src="https://code.highcharts.com/modules/wordcloud.js"></script>
-    <script src="https://code.highcharts.com/modules/exporting.js"></script>
-    <script src="https://code.highcharts.com/modules/export-data.js"></script>
-    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
-    <script src="https://code.highcharts.com/modules/data.js"></script>
-    <script src="https://code.highcharts.com/modules/drilldown.js"></script>
 
-</svelte:head>
 
 <script>
     // @ts-nocheck
